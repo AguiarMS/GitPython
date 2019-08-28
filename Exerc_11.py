5de5e8735a0e80a50024bc18600907e648b7cdb5
gitvessel <40027096+gitvessel@users.noreply.github.com>
2019-08-28 09:45:25 -0300

    Add files via upload

---
+++ Exerc_11.py
@@ -0,0 +1,4 @@
+custo_fabrica = float(input('Digite o custo de fábrica do carro: '))
+print(custo_fabrica)
+soma = (custo_fabrica * 0.28) + (custo_fabrica * 0.45) + (custo_fabrica)
+print('Custo da fabrica: ', soma)
